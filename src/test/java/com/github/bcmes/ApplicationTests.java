a8e96da62233af009ccd54206764dbd3bf2f97de
Bruno Silva <brunomeloesilva@gmail.com>
2024-10-31 21:39:28 -0400

    study: tests

--- src/test/java/com/github/bcmes/ApplicationTests.java
+++ src/test/java/com/github/bcmes/ApplicationTests.java
@@ -1,13 +1,13 @@
 package com.github.bcmes;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
 	}
 
 }
