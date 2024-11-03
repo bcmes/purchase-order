6b80674b156e5edd82948dee4fcbb00ca53db814
Bruno Silva <brunomeloesilva@gmail.com>
2024-11-03 00:52:40 -0400

    study: SonarCloud

--- src/test/java/com/github/bcmes/ProductControllerTest.java
+++ src/test/java/com/github/bcmes/ProductControllerTest.java
@@ -44,79 +44,79 @@ class ProductControllerTest {
         verify(productRepository, times(1)).findAll();
     }
 
-//    @Test
-//    void getOne_ShouldReturnProduct_WhenProductExists() throws Exception {
-//        // Arrange
-//        Long productId = 1L;
-//        ProductEntity product = new ProductEntity(productId, "Product Description");
-//        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
-//
-//        // Act & Assert
-//        mockMvc.perform(get("/products/{id}", productId))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id", is(1)))
-//                .andExpect(jsonPath("$.description", is("Product Description")));
-//
-//        verify(productRepository, times(1)).findById(productId);
-//    }
-//
-//    @Test
-//    void addOne_ShouldSaveProductAndReturnCreatedStatus() throws Exception {
-//        // Arrange
-//        ProductEntity productEntity = new ProductEntity(1L, "Product Description");
-//        when(productRepository.saveAndFlush(any(ProductEntity.class))).thenReturn(productEntity);
-//
-//        // Act & Assert
-//        mockMvc.perform(post("/products")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content("{\"description\": \"Product Description\"}"))
-//                .andExpect(status().isCreated())
-//                .andExpect(header().exists("Location"))
-//                .andExpect(jsonPath("$.id", is(1)))
-//                .andExpect(jsonPath("$.description", is("Product Description")));
-//
-//        verify(productRepository, times(1)).saveAndFlush(any(ProductEntity.class));
-//    }
-//
-//    @Test
-//    void deleteOne_ShouldDeleteProductAndReturnNoContentStatus() throws Exception {
-//        // Arrange
-//        Long productId = 1L;
-//        doNothing().when(productRepository).deleteById(productId);
-//
-//        // Act & Assert
-//        mockMvc.perform(delete("/products/{id}", productId))
-//                .andExpect(status().isNoContent());
-//
-//        verify(productRepository, times(1)).deleteById(productId);
-//    }
-//
-//    @Test
-//    void alterOne_ShouldUpdateProductAndReturnCreatedStatus() throws Exception {
-//        // Arrange
-//        ProductEntity productEntity = new ProductEntity(1L, "Updated Product Description");
-//        when(productRepository.saveAndFlush(any(ProductEntity.class))).thenReturn(productEntity);
-//
-//        // Act & Assert
-//        mockMvc.perform(put("/products")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content("{\"id\": 1, \"description\": \"Updated Product Description\"}"))
-//                .andExpect(status().isCreated())
-//                .andExpect(header().exists("Location"))
-//                .andExpect(jsonPath("$.id", is(1)))
-//                .andExpect(jsonPath("$.description", is("Updated Product Description")));
-//
-//        verify(productRepository, times(1)).saveAndFlush(any(ProductEntity.class));
-//    }
-//
-//    @Test
-//    void addOne_ShouldReturnBadRequest_WhenRequestIsInvalid() throws Exception {
-//        // Act & Assert
-//        mockMvc.perform(post("/products")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content("{\"description\": \"P\"}"))
-//                .andExpect(status().isBadRequest());
-//
-//        verify(productRepository, times(0)).saveAndFlush(any(ProductEntity.class));
-//    }
+    @Test
+    void getOne_ShouldReturnProduct_WhenProductExists() throws Exception {
+        // Arrange
+        Long productId = 1L;
+        ProductEntity product = new ProductEntity(productId, "Product Description");
+        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+
+        // Act & Assert
+        mockMvc.perform(get("/products/{id}", productId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.description", is("Product Description")));
+
+        verify(productRepository, times(1)).findById(productId);
+    }
+
+    @Test
+    void addOne_ShouldSaveProductAndReturnCreatedStatus() throws Exception {
+        // Arrange
+        ProductEntity productEntity = new ProductEntity(1L, "Product Description");
+        when(productRepository.saveAndFlush(any(ProductEntity.class))).thenReturn(productEntity);
+
+        // Act & Assert
+        mockMvc.perform(post("/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"description\": \"Product Description\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.description", is("Product Description")));
+
+        verify(productRepository, times(1)).saveAndFlush(any(ProductEntity.class));
+    }
+
+    @Test
+    void deleteOne_ShouldDeleteProductAndReturnNoContentStatus() throws Exception {
+        // Arrange
+        Long productId = 1L;
+        doNothing().when(productRepository).deleteById(productId);
+
+        // Act & Assert
+        mockMvc.perform(delete("/products/{id}", productId))
+                .andExpect(status().isNoContent());
+
+        verify(productRepository, times(1)).deleteById(productId);
+    }
+
+    @Test
+    void alterOne_ShouldUpdateProductAndReturnCreatedStatus() throws Exception {
+        // Arrange
+        ProductEntity productEntity = new ProductEntity(1L, "Updated Product Description");
+        when(productRepository.saveAndFlush(any(ProductEntity.class))).thenReturn(productEntity);
+
+        // Act & Assert
+        mockMvc.perform(put("/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"id\": 1, \"description\": \"Updated Product Description\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.description", is("Updated Product Description")));
+
+        verify(productRepository, times(1)).saveAndFlush(any(ProductEntity.class));
+    }
+
+    @Test
+    void addOne_ShouldReturnBadRequest_WhenRequestIsInvalid() throws Exception {
+        // Act & Assert
+        mockMvc.perform(post("/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"description\": \"P\"}"))
+                .andExpect(status().isBadRequest());
+
+        verify(productRepository, times(0)).saveAndFlush(any(ProductEntity.class));
+    }
 }
