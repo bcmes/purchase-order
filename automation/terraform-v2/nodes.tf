4ccc7efa0ca1a4b7a2a3cf305f1d2953ccaaca92
Bruno Silva <brunomeloesilva@gmail.com>
2024-11-30 23:33:53 -0400

    terraform-aws: criando nodes

--- automation/terraform-v2/nodes.tf
+++ automation/terraform-v2/nodes.tf
@@ -41,16 +41,17 @@ resource "aws_eks_node_group" "my-eks-node-group-1" {
   node_group_name = "my-eks-node-group-1"
   node_role_arn   = aws_iam_role.my-role-node.arn
   subnet_ids      = aws_subnet.my-subnet[*].id
+  instance_types = ["t3.micro"]
 
   scaling_config {
-    desired_size = 2
-    max_size     = 4
+    desired_size = 2 #neste node-group sempre terei 2 maquinas de pé
+    max_size     = 4 #se o bixo pegar, sobe pra 4
     min_size     = 2
   }
 
-  # update_config {
-  #   max_unavailable = 1
-  # }
+  update_config {
+    max_unavailable = 1
+  }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
