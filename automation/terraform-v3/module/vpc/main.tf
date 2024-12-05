09a7d387a7a173d8484b420e68bb7f802c2a04f5
Bruno Silva <brunomeloesilva@gmail.com>
2024-12-05 19:53:09 -0400

    terraform-aws: comunicando com k8s na aws

--- automation/terraform-v3/module/vpc/main.tf
+++ automation/terraform-v3/module/vpc/main.tf
@@ -1,6 +1,6 @@
 # Create a VPC
 resource "aws_vpc" "my-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16" #Esse cidr_block é o Network do https://jodies.de/ipcalc
   tags = {
     Name = "${var.prefix}-vpc-example"
   }
@@ -11,9 +11,9 @@ data "aws_availability_zones" "available" {}
 resource "aws_subnet" "my-subnet" {
   count             = 2
   vpc_id            = aws_vpc.my-vpc.id
-  cidr_block        = "10.0.${count.index}.0/24"
+  cidr_block        = "10.0.${count.index}.0/24" #Esse cidr_block é o Network do https://jodies.de/ipcalc
   availability_zone = data.aws_availability_zones.available.names[count.index]
-  map_public_ip_on_launch = true #todo recurso dentro dessas subnets já gera um IP publico
+  map_public_ip_on_launch = true #toodo recurso dentro dessas subnets já gera um IP publico
 
   tags = {
     Name = "${var.prefix}-subnet-example-${count.index + 1}"
